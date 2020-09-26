2646f0d1c6a9c63b97f091f19a017a1fe7b47b65
Joonas Pääkkö <paakkoj@gmail.com>
2020-09-26 16:44:43 +0300

    Irregular whitespace fix. Related to #87

--- source/assets/js/dialog.builder/modules/toolbar/export/make.item.js
+++ source/assets/js/dialog.builder/modules/toolbar/export/make.item.js
@@ -53,7 +53,7 @@ function makeJSXitem( index, data, jsxParents, type, id, parentId, parentType, s
 			var defaultValue = defaultCreationProps[ prop ];
 			if ( defaultValue != value ) {
 				var isString = typeof value == 'string';
-				var doubleQuotes = (isString && prop !== 'numberOfColumns' && prop !== 'columnTitles' && prop !== 'columnWidths') ? '"' : '';
+				var doubleQuotes = (isString && prop !== 'numberOfColumns' && prop !== 'columnTitles' && prop !== 'columnWidths') ? '"' : '';
 				userCProps += ', ' + prop + ': ' + doubleQuotes + value + doubleQuotes;
 			}
 		});
